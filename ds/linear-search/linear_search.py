ce3eedb72dffad97c99243de186ebae02af9b719
mazharul.miraz <mazharul.miraz@outlook.com>
2019-05-29 15:16:10 +0530

    bug fix in ls algo

--- ds/linear-search/linear_search.py
+++ ds/linear-search/linear_search.py
@@ -1,27 +1,42 @@
-#linear search 
-#WAP search on selement in list
+# #linear search 
+# #WAP search on selement in list
 
-import pdb
-def linear_search(List,key):
-	#loop init
-	for i in range (len(List)):
-		# pdb.set_trace()
-		if (List[i] == key):
-			return i
-			break
-	return -1
+# import pdb
+# def linear_search(List,key):
+# 	#loop init
+# 	for i in range (len(List)):
+# 		# pdb.set_trace()
+# 		if (List[i] == key):
+# 			return i;
+# 			break;
+# 	return -1
 
-#given list
+# #given list
 
-List=[2,3,4,6,9,10,11,14,18,19,22,24,50,56,60,70,80,85,90,100]
+# List=[2,3,4,6,9,10,11,14,18,19,22,24,50,56,60,70,80,85,90,100]
 
-r = linear_search(List,23)
+# r = linear_search(List,23)
 
-key =input("Enter the search key:  ")
+# key =input("Enter the search key:  ")
 
 
-if (r!=1):
-	print(key,'Search key findout',i)
+# if (r!=-1):
+# 	print(key,'Search key findout',r)
+# else:
+# 	print(key,'key is not found')
+
+
+def search(arr,x):
+	for i in range (len(arr)):
+		if (arr[i] == x):
+			return i;
+	return -1;
+
+arr =[2,5,7,9,15]
+x = int(input("enter search key: "))
+result = search(arr,x)
+
+if(result == -1):
+	print("element is not in arry")
 else:
-	print('key is not found')
-
+	print("element is present at index",result)
